0872ea1ea151c21aabfaee7abef8ca6de157b4df
ch0556740056 <ch0556740056@gmail.com>
2023-05-15 00:05:34 +0300

    last

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-// import  express  from 'express';
+import  express  from 'express';
+import { createRequire } from 'module';
 
-// const app=express()
-// const port=3000
 
+const require = createRequire(import.meta.url);
+
+const app=express()
+const port=3000
 const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
 sdk.auth('rnd_RMSMZ9FyWt6ImW4Ooj0suRxLNbmd');
@@ -12,8 +15,16 @@ sdk.getServices({limit: '20'})
 
 
 
+  app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+app.listen(port,()=>{
+    console.log(`app listening on http://localhost:${port}`)
+})
+console.log("hi")
 
-// app.get('/',(req,res)=>{
-// res.send("hygh")
-// })
 // rnd_b2JZFpRsYji4DMjXtN4t7rig5S5J
+
+
+
+
