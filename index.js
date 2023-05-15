92a48f83c709b83b35a2ec5e6d2eabf869c7a6b4
ch0556740056 <ch0556740056@gmail.com>
2023-05-15 17:37:57 +0300

    getApp

--- index.js
+++ index.js
@@ -6,22 +6,22 @@ const require = createRequire(import.meta.url);
 
 const app=express()
 const port=3000
-const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
-sdk.auth('rnd_RMSMZ9FyWt6ImW4Ooj0suRxLNbmd');
-sdk.getServices({limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
 
 
   app.get('/',(req,res)=>{
-    res.send('hello world')
+    sdk.auth('rnd_RMSMZ9FyWt6ImW4Ooj0suRxLNbmd');
+   sdk.getServices({limit: '20'})
+  .then(({ data }) => res.send(data))
+  .catch(err => console.error(err));
+
+
 })
 app.listen(port,()=>{
     console.log(`app listening on http://localhost:${port}`)
 })
-console.log("hi")
 
 // rnd_b2JZFpRsYji4DMjXtN4t7rig5S5J
 
